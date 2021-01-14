23a3ad5b9dc5d683ca47802527eb40c92a87e32f
Darko <darko_trpevski@yahoo.com>
2021-01-14 15:12:04 +0100

    Changed Alert Component to give color on the alerts depending on status

--- client/src/layouts/Alert/Alert.tsx
+++ client/src/layouts/Alert/Alert.tsx
@@ -1,21 +1,38 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { AlertType } from '../../redux/types/types';
+import { removeAlert } from '../../redux/alert/alertActions';
+import { Alert as ChakraAlert, AlertIcon, AlertTitle, AlertDescription, CloseButton } from "@chakra-ui/core";
 
 interface AlertProps {
-  alerts: AlertType[]
+  alerts: AlertType[],
+  removeAlert: (id: string) => void,
 }
 
-const Alert: React.FC<AlertProps> = ({ alerts }) => {
+const Alert: React.FC<AlertProps> = ({ alerts, removeAlert }) => {
+
+  // return (
+  //   <>
+  //     {alerts !== null &&
+  //     alerts.length > 0 &&
+  //     alerts.map(alert => (
+  //       <div key={alert.id} className={`alert alert-${alert.alertType}`}>
+  //         {alert.msg}
+  //       </div>
+  //     ))}
+  //   </>
+  // )
 
   return (
     <>
       {alerts !== null &&
       alerts.length > 0 &&
       alerts.map(alert => (
-        <div key={alert.id} className={`alert alert-${alert.alertType}`}>
-          {alert.msg}
-        </div>
+        <ChakraAlert key = {alert.id} status={alert.alertType}>
+          <AlertIcon />
+          <AlertTitle mr={2}>{alert.msg}</AlertTitle>
+          <CloseButton onClick={() => removeAlert(alert.id)} position="absolute" right="8px" top="8px" />
+        </ChakraAlert>
       ))}
     </>
   )
@@ -26,4 +43,4 @@ const mapStateToProps = (state: any) => ({
   alerts: state.alertReducer
 });
 
-export default connect(mapStateToProps)(Alert);
+export default connect(mapStateToProps, { removeAlert })(Alert);
